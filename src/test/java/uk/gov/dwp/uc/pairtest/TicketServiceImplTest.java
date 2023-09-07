c446c7d65a43053c19d3df282ec721d1c687b9e3
S-Inferno <singsanjaybisht@gmail.com>
2023-09-07 10:47:23 +0100

    more test cases

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -15,7 +15,7 @@ public class TicketServiceImplTest {
     }
 
     @Test()
-    public void test_purchaseTickets_HappyPath() {
+    public void test_purchaseTickets_happyPath() {
         TicketTypeRequest ticketTypeRequestAdult = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 10);
         TicketTypeRequest ticketTypeRequestChild = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 5);
         TicketTypeRequest ticketTypeRequestInfant = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2);
@@ -40,7 +40,7 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void test_purchaseTicket_ChildAndInfant() {
+    public void test_purchaseTicket_childAndInfant() {
         TicketTypeRequest ticketTypeRequestChild = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 5);
         TicketTypeRequest ticketTypeRequestInfant = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2);
 
@@ -52,7 +52,7 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void test_purchaseTicket_ChildAndInfant_MaxTicket() {
+    public void test_purchaseTicket_childAndInfant_maxTicket() {
         TicketTypeRequest ticketTypeRequestChild = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 5);
         TicketTypeRequest ticketTypeRequestInfant = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 20);
 
@@ -69,7 +69,7 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void test_purchaseTicket_onlyAdult_MaxTicket() {
+    public void test_purchaseTicket_onlyAdult_maxTicket() {
         TicketTypeRequest ticketTypeRequestAdult = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 50);
         InvalidPurchaseException invalidPurchaseException = Assert.assertThrows(InvalidPurchaseException.class,
                 () -> ticketService.purchaseTickets(1L, ticketTypeRequestAdult)
@@ -78,7 +78,7 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void test_purchaseTicket_MaxTicket() {
+    public void test_purchaseTicket_maxTicket() {
         TicketTypeRequest ticketTypeRequestAdult = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 10);
         TicketTypeRequest ticketTypeRequestChild = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 50);
         TicketTypeRequest ticketTypeRequestInfant = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2);
@@ -89,14 +89,14 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void test_purchaseTicket_SameType() {
+    public void test_purchaseTicket_sameType() {
         TicketTypeRequest ticketTypeRequestA = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
         TicketTypeRequest ticketTypeRequestB = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
         TicketTypeRequest ticketTypeRequestC = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
         ticketService.purchaseTickets(1L, ticketTypeRequestA, ticketTypeRequestB, ticketTypeRequestC);
     }
     @Test
-    public void test_purchaseTicket_SameType_MaxTicket() {
+    public void test_purchaseTicket_sameType_maxTicket() {
         TicketTypeRequest ticketTypeRequestA = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 20);
         TicketTypeRequest ticketTypeRequestB = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
         TicketTypeRequest ticketTypeRequestC = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
@@ -104,5 +104,21 @@ public class TicketServiceImplTest {
                 () ->         ticketService.purchaseTickets(1L, ticketTypeRequestA, ticketTypeRequestB, ticketTypeRequestC)
         );
         Assert.assertTrue(invalidPurchaseException.getMessage().contains("Max ticket allowed to be purchased is 20."));
+    }
+
+    @Test
+    public void test_purchaseTicket_noTicket(){
+        InvalidPurchaseException invalidPurchaseException = Assert.assertThrows(InvalidPurchaseException.class,
+                () ->         ticketService.purchaseTickets(1L)
+        );
+        Assert.assertTrue(invalidPurchaseException.getMessage().contains("Please add at least 1 Adult ticket."));
+    }
+
+    @Test
+    public void test_purchaseTicket_invalidAccount(){
+        InvalidPurchaseException invalidPurchaseException = Assert.assertThrows(InvalidPurchaseException.class,
+                () ->         ticketService.purchaseTickets(0L)
+        );
+        Assert.assertTrue(invalidPurchaseException.getMessage().contains("Invalid Account."));
     }
 }
